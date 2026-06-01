e6368ec8f46ad554304fe8c5f81135c53f536826
Md. Mamun-Or-Rashid <mamun.techbuzz@gmail.com>
2026-06-01 16:33:10 +0600

    code refactoring using AI

--- C_2_Seating_Arrangement_Hard_Version.cpp
+++ C_2_Seating_Arrangement_Hard_Version.cpp
@@ -1,84 +1,68 @@
 #pragma GCC optimize("Ofast")
 #include <bits/stdc++.h>
 using namespace std;
+
 #define int long long
 
-void Solve()
-{
-    int n, x, s; cin>>n>>x>>s;
-    string str; cin >> str;
-    int eCnt = 0, aCnt = 0, Icnt = 0;
-    for(int i = 0; i<n; i++) {
-      if(str[i] == 'E' && (aCnt || Icnt)) eCnt++;
-      if(str[i] == 'A') {
-        aCnt++;
-      }
-      if(str[i] == 'I') Icnt++;
-    }
+void Solve() {
+    int n, x, s;
+    cin >> n >> x >> s;
 
+    string str;
+    cin >> str;
 
+    int introverts = count(str.begin(), str.end(), 'I');
 
     int ans = 0;
     int space = 0;
-    aCnt = 0;
-    for(int i = 0; i<n;  i++) {
-      if(str[i]=='I') {
-        if(x) {
-          ans += 1;
-          x--;
-          space += (s-1);
-          Icnt--;
-        }
-      }
-      else {
-        if(str[i] == 'A') {
+    int ambiInside = 0;
 
-          if(x && (x - 1 >= Icnt || !space)) {
-            x--;
-            space += (s-1);
-            ans++;
-          }
-          else {
-              if(space){
-                aCnt++;
-                space--;
-                ans++;
-              }
-          }
-        }
-        else if(str[i] == 'E') {
-          if(space) {
-            space--;
-            ans++;
-          }else if(aCnt && x) {
-            space+=(s-1);
-            aCnt--;
-            ans++;
-            x--;
-          }
+    for (char c : str) {
+        if (c == 'I') {
+            if (x) {
+                --x;
+                --introverts;
+                ++ans;
+                space += s - 1;
+            }
+            continue;
         }
 
-      }
+        if (c == 'A') {
+            if (x && (x - 1 >= introverts || !space)) {
+                --x;
+                ++ans;
+                space += s - 1;
+            } else if (space) {
+                --space;
+                ++ambiInside;
+                ++ans;
+            }
+            continue;
+        }
+
+        if (space) {
+            --space;
+            ++ans;
+        } else if (ambiInside && x) {
+            --ambiInside;
+            --x;
+            ++ans;
+            space += s - 1;
+        }
     }
 
-    cout<<ans<<'\n';
-
-
-
+    cout << ans << '\n';
 }
 
-int32_t main()
-{
-    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-    int t = 1;
+    int t;
     cin >> t;
 
-    for (int i = 1; i <= t; i++)
-    {
-        // cout << "Case #" << i << ": ";
-        Solve();
-    }
+    while (t--) Solve();
 
     return 0;
 }
